8bfed5104d45df6f0de50726fa2122d7fd06fe65
bijaybudha-1 <bijay.budhasp25@cps.edu.np>
2025-12-02 15:44:26 +0545

    Adding Function Problem Solution

---
+++ 05_Function Problem/02_sum_two_numbers.py
@@ -0,0 +1,10 @@
+# Function With Multiple Parameter
+# Problem: Create a function that takes two number as parameters and returns their sum.
+from Crypto.Util import number
+
+
+def sum_two_numbers (num1, num2):
+    return num1 + num2
+
+result = sum_two_numbers(5, 4)
+print(f"The sum of two numbers is: {result}")
